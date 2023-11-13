241ffc20799dc1727e3086f7a89381e7fe2a4c9f
Mihail Vorobyev <1viklover@gmail.com>
2023-11-13 20:35:34 +0500

    Initial commit

---
+++ src/main/kotlin/ru/viklover/cards/LoyaltyProgramCardServiceApplication.kt
@@ -0,0 +1,11 @@
+package ru.viklover.cards
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LoyaltyProgramCardServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<LoyaltyProgramCardServiceApplication>(*args)
+}
---
+++ src/test/kotlin/ru/viklover/cards/LoyaltyProgramCardServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.viklover.cards
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LoyaltyProgramCardServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
