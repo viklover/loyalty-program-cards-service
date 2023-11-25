055c15391a3dc8de458d86381661d913a4cb3006
Mihail Vorobyev <1viklover@gmail.com>
2023-11-25 14:55:03 +0500

    [INIT] OpenAPI Contracts generation attempt

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.1.5"
     id("io.spring.dependency-management") version "1.1.3"
+    id("org.openapi.generator") version "6.6.0"
     kotlin("jvm") version "1.8.22"
     kotlin("plugin.spring") version "1.8.22"
 }
@@ -27,6 +28,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     implementation("org.springframework:spring-jdbc")
+    implementation("org.openapitools:openapi-generator-gradle-plugin:6.6.0")
     runtimeOnly("org.postgresql:postgresql")
     runtimeOnly("org.postgresql:r2dbc-postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
@@ -38,7 +40,32 @@ tasks.withType<KotlinCompile> {
         freeCompilerArgs += "-Xjsr305=strict"
         jvmTarget = "17"
     }
+    dependsOn("openApiGenerate")
 }
+
+val generatedSourcesPath = "$buildDir/generated"
+
+openApiGenerate {
+    generatorName.set("kotlin-spring")
+    inputSpec.set("$rootDir/src/main/resources/contracts/endpoints.yaml")
+    outputDir.set(generatedSourcesPath)
+    apiPackage.set("ru.viklover.cards.contracts.controller")
+    invokerPackage.set("ru.viklover.cards.contracts")
+    modelPackage.set("ru.viklover.cards.contracts.models")
+    packageName.set("ru.viklover.cards.contracts")
+    configOptions.set(
+        mapOf(
+            "reactive" to "true",
+            "useSpringBoot3" to "true",
+            "useSwaggerUI" to "false",
+            "interfaceOnly" to "false",
+            "annotationLibrary" to "none",
+            "packageName" to "ru.viklover.cards.contracts"
+        )
+    )
+}
+
+kotlin.sourceSets["main"].kotlin.srcDir("$generatedSourcesPath/src/main/kotlin")
 
 // tasks.withType<Test> {
 //     useJUnitPlatform()
---
+++ src/main/kotlin/ru/viklover/cards/api/v1/controller/PetController.kt
@@ -0,0 +1,7 @@
+package ru.viklover.cards.api.v1.controller
+//
+//import ru.viklover.cards.contracts.controller.PetAPIsApi
+//
+//class PetController : PetAPIsApi {
+//
+//}
