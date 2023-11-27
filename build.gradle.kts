a70d1a292f51f9e821c6ae35f79241d2bdcfb44d
Mihail Vorobyev <1viklover@gmail.com>
2023-11-27 13:59:28 +0500

    [INIT] Configure gradle for openapi contracts generation (controllers interfaces, dto)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
 
 plugins {
     id("org.springframework.boot") version "3.1.5"
@@ -36,37 +37,20 @@ dependencies {
     testImplementation("io.projectreactor:reactor-test")
 }
 
-val artifactId = rootProject.name
+val projectPackage = "$group.${rootProject.name}"
+
 val generatedSourcesPath = "${layout.buildDirectory.get()}/generated"
+val contractsDirectoryPath = "$rootDir/src/main/resources/contracts"
+val contractTasks = apiContracts()
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs += "-Xjsr305=strict"
         jvmTarget = "17"
     }
-    dependsOn("openApiGenerate")
+    dependsOn(contractTasks)
 }
 
-openApiGenerate {
-    generatorName.set("kotlin-spring")
-    inputSpec.set("$rootDir/src/main/resources/contracts/api-v1.yaml")
-    outputDir.set(generatedSourcesPath)
-    apiPackage.set("$group.$artifactId.contracts.v1.controller")
-    modelPackage.set("$group.$artifactId.contracts.v1.models")
-    packageName.set("$group.$artifactId.contracts.v1")
-    configOptions.set(
-        mapOf(
-            "reactive" to "true",
-            "useSpringBoot3" to "true",
-            "useSwaggerUI" to "false",
-            "interfaceOnly" to "true",
-            "apiSuffix" to "V1",
-            "annotationLibrary" to "none",
-            "packageName" to "$group.$artifactId.contracts",
-            "documentationProvider" to "none"
-        )
-    )
-}
 
 kotlin.sourceSets["main"].kotlin.srcDir("$generatedSourcesPath/src/main/kotlin")
 
@@ -76,4 +60,50 @@ kotlin.sourceSets["main"].kotlin.srcDir("$generatedSourcesPath/src/main/kotlin")
 
 tasks.bootBuildImage {
     builder.set("paketobuildpacks/builder-jammy-base:latest")
+}
+
+/* GENERATE API CONTRACT TASKS */
+
+fun apiContracts(): List<String> {
+
+    val directory = file(contractsDirectoryPath)
+    val files = directory.listFiles() ?: throw StopExecutionException("Where's contracts?")
+
+    val pattern = Regex("api-v(\\d+\\.\\d+)\\.yaml")
+
+    return files.map { file ->
+
+        val matches = pattern.find(file.name) ?: throw StopExecutionException("Matches were not found")
+
+        val apiSuffix = matches.let {
+            val (major, minor) = it.groupValues[1].split('.')
+
+            if (minor == "0") "v$major" else "v${major}_${minor}"
+        }
+
+        val taskName = "generate${apiSuffix.uppercase()}"
+
+        tasks.create(taskName, GenerateTask::class) {
+            generatorName.set("kotlin-spring")
+            inputSpec.set("$rootDir/src/main/resources/contracts/${file.name}")
+            outputDir.set(generatedSourcesPath)
+            apiPackage.set("$projectPackage.contracts.$apiSuffix.controller")
+            modelPackage.set("$projectPackage.contracts.$apiSuffix.models")
+            packageName.set("$projectPackage.contracts.$apiSuffix")
+            configOptions.set(
+                mapOf(
+                    "reactive" to "true",
+                    "useSpringBoot3" to "true",
+                    "useSwaggerUI" to "false",
+                    "interfaceOnly" to "true",
+                    "apiSuffix" to "Controller${apiSuffix.uppercase()}",
+                    "annotationLibrary" to "none",
+                    "packageName" to "$projectPackage.contracts",
+                    "documentationProvider" to "none"
+                )
+            )
+        }
+
+        return@map taskName;
+    }
 }
